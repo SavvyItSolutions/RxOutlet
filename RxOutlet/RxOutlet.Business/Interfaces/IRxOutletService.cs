acc1e607e0b3b36466fc31894ae90f3d243901c4
Soujanya13 <soujanyareddy.gade@gmail.com>
2017-03-30 11:43:53 +0530

    Added Models, DataAccess, Bussiness Folders and Linq to Sql Class for DB Connection.

---
+++ RxOutlet/RxOutlet.Business/Interfaces/IRxOutletService.cs
@@ -0,0 +1,19 @@
+﻿using RxOutlet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace RxOutlet.Business
+{
+    public interface IRxOutletService
+    {
+
+        RxOutlet_MenuListRespone GetMenuList();
+        RxOutlet_SubMenuListResponse GetSubMenuList(int menuID);
+        RxOutLet_MenuItemListResponse GetMenuItemList(int menuID, int subMenuID);
+    }
+}
