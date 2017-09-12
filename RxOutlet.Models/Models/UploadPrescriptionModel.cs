45c8d440de8190dbe8111417d9a3d9af3fad6cb5
Soumik Paul <soumik.12paul@gmail.com>
2017-09-12 17:23:29 +0530

    Email for prescription upload

--- RxOutlet.Models/Models/UploadPrescriptionModel.cs
+++ RxOutlet.Models/Models/UploadPrescriptionModel.cs
@@ -11,6 +11,7 @@ namespace RxOutlet.Models
 {
    public class UploadPrescriptionModel
     {
+
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Name { get; set; }
