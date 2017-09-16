dadd5a26ebc351ff125c02572c83b6af3e456be6
Soujanya13 <soujanyareddy.gade@gmail.com>
2017-09-16 16:47:54 +0530

    Byte Array

--- RxOutlet.Models/Models/UploadPrescriptionModel.cs
+++ RxOutlet.Models/Models/UploadPrescriptionModel.cs
@@ -11,7 +11,9 @@ namespace RxOutlet.Models
 {
    public class UploadPrescriptionModel
     {
-                  public string Email { get; set; }
+
+
+        public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Name { get; set; }
         public string Title { get; set; }
